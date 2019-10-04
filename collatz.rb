76d6418ae670c13c38ca1a983233b6511b2891ff
Roberto Lombilla <ironmanbomb@hotmail.com>
2019-10-04 19:45:08 +0000

    collatz.rb

--- collatz.rb
+++ collatz.rb
@@ -1,35 +1,26 @@
-def collatz( input_number)
-  lenght = 1
-  if input_number < 0
-    raise ArgumentError.new 'Invalid. Only positive numbers'
+def collatz(n)
+  array = []
+  array[0] = n
+  if n > 1 
+    while n > 1
+      if n.even?
+        n = n/2
+      else
+        n = 3*n + 1
+      end
+      array.push(n)
+    end
   end
-
-  while input_number != 1
-    lenght += 1
-    input_number = input_number.even? ? (input_number / 2) : (input_number * 3 + 1)
-    
-  end
-  lenght
+  return array.length
 end
 
-def max_collatz
-  largest_len = 0
-  largest_number = 0
-  (1 .. 1000000).each do |n|
-    lenght = collatz(n)
-    if lenght > largest_len
-      largest_len = lenght
-      largest_number = n
-    end
-
-    class TestCollatz < Test::Unit::TestCase
-      def test_collatz_sequence
-        assert(collatz( input_number 1) = 1)
-        assert(collatz( input_number 2) = 2)
-        assert(collatz( input_number 3) = 8)
-        assert(collatz( input_number 4) = 3)
-        assert(collatz( input_number 5) = 6)
-        assert(collatz( input_number 6) = 9)
-        assert(collatz( input_number 7) = 17)
-      end
-    end
+longest = 0
+num = 1
+while num <= 1000000
+  clen = collatz(num)
+  if clen >= longest 
+    longest = clen
+  end
+  num += 1
+end
+puts longest
